8ccb13e45900cb9cec681ddc3ffa026a2d1b37e0
Carl Brasic <carl.brasic@peopleadmin.com>
2013-07-30 21:13:08 -0500

    refactor

--- main.go
+++ main.go
@@ -1,49 +1,63 @@
 package main
 
 import (
-  "flag"
   "fmt"
+  "time"
   "io/ioutil"
   "launchpad.net/goamz/aws"
   "launchpad.net/goamz/s3"
 )
 
 var (
-  bucketName string
-  fileName   string
+  bucketName = "pa-hrsuite-production"
+  basedir = "/tmp/"
+  keybase = "606/docs/"
+  doclist = []string{"100001.pdf","100009.pdf","100030.pdf","100035.pdf"}
 )
 
-func init() {
-  flag.StringVar(&bucketName, "b", "", "Bucket Name")
-}
-
-func getFile(name string, c chan []byte) {
-  auth, err := aws.EnvAuth()
-  if err != nil {
-    panic(err.Error())
-  }
-  s := s3.New(auth, aws.USEast)
-  bucket := s.Bucket(bucketName)
-
-  data, err := bucket.Get(name)
+func getFile(bucket *s3.Bucket, docname string, c chan int) {
+  s3key := keybase + docname
+  data, err := bucket.Get(s3key)
   if err != nil { panic(err) }
-  fmt.Println(len(data))
 
-  path := "/tmp/"+name
+  path := "/tmp/"+docname
   err = ioutil.WriteFile(path, data, 0644)
   if err != nil { panic(err) }
-  c <- data
+  c <- len(data)
 
+}
+
+func connect() *s3.Bucket {
+  auth, err := aws.EnvAuth()
+  if err != nil { panic(err) }
+  s := s3.New(auth, aws.USEast)
+  return s.Bucket(bucketName)
+}
+
+func printSummary(start time.Time, bytes int){
+  elapsed := time.Since(start)
+  seconds := elapsed.Seconds()
+  mbps := float64(bytes) / 1024 / 1024 / seconds
+  fmt.Printf("got %d bytes over %d files in %f secs (%f MB/s)\n",
+             bytes, len(doclist), seconds, mbps)
 }
 
 
 func main() {
 
-  flag.Parse()
-  c := make(chan []byte)
-  go getFile("000.txt",c)
-  go getFile("000.txt",c)
-  fmt.Printf("got %d bytes\n",len(<-c))
-  fmt.Printf("got %d bytes\n",len(<-c))
+  start := time.Now()
+  bucket := connect()
+  c := make(chan int)
+  for _,doc := range doclist{
+    go getFile(bucket,doc,c)
+  }
+
+  totalBytes := 0
+  for _,doc := range doclist{
+    recieved := <-c
+    fmt.Printf("%s was %d bytes\n", doc,recieved)
+    totalBytes += recieved
+  }
+  printSummary(start,totalBytes)
 
 }
