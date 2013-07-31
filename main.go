eba6db9d950d903afac15192ab11ccda353b5593
Carl Brasic <carl.brasic@peopleadmin.com>
2013-07-30 19:52:01 -0500

    working in parallel

--- main.go
+++ main.go
@@ -17,30 +17,31 @@ func init() {
 }
 
 func getFile(name string, c chan []byte) {
+  auth, err := aws.EnvAuth()
+  if err != nil {
+    panic(err.Error())
+  }
+  s := s3.New(auth, aws.USEast)
+  bucket := s.Bucket(bucketName)
+
+  data, err := bucket.Get(name)
+  fmt.Println(len(data))
+  if err != nil {
+    panic(err.Error())
+  }
+  c <- data
 
 }
-
 
 
 func main() {
 
   flag.Parse()
+  c := make(chan []byte)
+  go getFile("000.txt",c)
+  go getFile("000.txt",c)
+  fmt.Printf("%s\n",<-c)
+  fmt.Printf("%s\n",<-c)
 
-  // The AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables are used.
-  auth, err := aws.EnvAuth()
-  if err != nil {
-    panic(err.Error())
-  }
 
-  // Open Bucket
-  s := s3.New(auth, aws.USEast)
-  bucket := s.Bucket(bucketName)
-
-  //#var data  []byte
-  data, err := bucket.Get("000.txt")
-  fmt.Println(len(data))
-  fmt.Printf("%s\n",data)
-  if err != nil {
-    panic(err.Error())
-  }
 }
