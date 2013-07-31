0d462558561ea643c3e4c13953c9051e36820b85
Carl Brasic <carl.brasic@peopleadmin.com>
2013-07-30 20:05:17 -0500

    write to a file

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "flag"
   "fmt"
+  "io/ioutil"
   "launchpad.net/goamz/aws"
   "launchpad.net/goamz/s3"
 )
@@ -25,10 +26,12 @@ func getFile(name string, c chan []byte) {
   bucket := s.Bucket(bucketName)
 
   data, err := bucket.Get(name)
+  if err != nil { panic(err) }
   fmt.Println(len(data))
-  if err != nil {
-    panic(err.Error())
-  }
+
+  path := "/tmp/"+name
+  err = ioutil.WriteFile(path, data, 0644)
+  if err != nil { panic(err) }
   c <- data
 
 }
@@ -40,8 +43,7 @@ func main() {
   c := make(chan []byte)
   go getFile("000.txt",c)
   go getFile("000.txt",c)
-  fmt.Printf("%s\n",<-c)
-  fmt.Printf("%s\n",<-c)
-
+  fmt.Printf("got %d bytes\n",len(<-c))
+  fmt.Printf("got %d bytes\n",len(<-c))
 
 }
