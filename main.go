470d9cea8128e1b8f13cf3b25789dd870260acab
Carl Brasic <carl.brasic@peopleadmin.com>
2013-08-02 14:54:56 -0500

    Split server methods

--- main.go
+++ main.go
@@ -2,48 +2,13 @@
 package main
 
 import (
-  "pdfcombiner/combiner"
+  "pdfcombiner/server"
   "net/http"
-  "net/url"
-  "fmt"
 )
 
-// Validate that the required URL params are present and correct.
-func check_params(params map[string] []string ) (docs []string, callback string, ok bool) {
-  callbacks := params["callback"]
-  if len(callbacks) < 1 {
-    return
-  }
-  callback = callbacks[0]
-  _, parseErr := url.Parse(callback)
-  docs, docsPresent := params["docs"]
-  ok = docsPresent && (parseErr == nil)
-  return
-}
-
-// Looks for one or more ?docs=FILE params and if found starts combination.
-func combineEndpoint(w http.ResponseWriter, r *http.Request) {
-  r.ParseForm()
-  params := r.Form
-  docs, callback, ok := check_params(params)
-  if !ok {
-    http.Error(w, "Need some docs and a callback url", http.StatusBadRequest)
-    return
-  }
-  request := &pdfcombiner.CombineRequest{
-    BucketName: "pa-hrsuite-production",
-    EmployerId: "606",
-    DocList: docs,
-    Callback: callback }
-  go pdfcombiner.Combine(request)
-  fmt.Fprintln(w, "Started combination on",docs)
-}
-
-func noopEndpoint(w http.ResponseWriter, r *http.Request) {}
-
 func main() {
-  http.HandleFunc("/favicon.ico", noopEndpoint)
-  http.HandleFunc("/health_check.html", noopEndpoint)
-  http.HandleFunc("/", combineEndpoint)
+  http.HandleFunc("/favicon.ico", pdfcombiner.NoopEndpoint)
+  http.HandleFunc("/health_check.html", pdfcombiner.NoopEndpoint)
+  http.HandleFunc("/", pdfcombiner.CombineEndpoint)
   http.ListenAndServe(":8080", nil)
 }
---
+++ server/server.go
@@ -0,0 +1,41 @@
+package pdfcombiner
+
+import (
+  "pdfcombiner/combiner"
+  "net/http"
+  "net/url"
+  "fmt"
+)
+
+// Validate that the required URL params are present and correct.
+func check_params(params map[string] []string ) (docs []string, callback string, ok bool) {
+  callbacks := params["callback"]
+  if len(callbacks) < 1 {
+    return
+  }
+  callback = callbacks[0]
+  _, parseErr := url.Parse(callback)
+  docs, docsPresent := params["docs"]
+  ok = docsPresent && (parseErr == nil)
+  return
+}
+
+// Looks for one or more ?docs=FILE params and if found starts combination.
+func CombineEndpoint(w http.ResponseWriter, r *http.Request) {
+  r.ParseForm()
+  params := r.Form
+  docs, callback, ok := check_params(params)
+  if !ok {
+    http.Error(w, "Need some docs and a callback url", http.StatusBadRequest)
+    return
+  }
+  request := &pdfcombiner.CombineRequest{
+    BucketName: "pa-hrsuite-production",
+    EmployerId: "606",
+    DocList: docs,
+    Callback: callback }
+  go pdfcombiner.Combine(request)
+  fmt.Fprintln(w, "Started combination on",docs)
+}
+
+func NoopEndpoint(w http.ResponseWriter, r *http.Request) {}
