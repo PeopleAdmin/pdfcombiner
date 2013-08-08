69f25af0ebdd821e062fb9212c949fae4b2ebf26
Carl Brasic <carl.brasic@peopleadmin.com>
2013-08-07 20:21:24 -0500

    Doc and remove extra variable

--- server/server.go
+++ server/server.go
@@ -1,10 +1,11 @@
+// Package server contains handlers which respond to requests to combine
+// pdf files and hands them off to the combiner package.
 package server
 
 import (
   "pdfcombiner/combiner"
   "pdfcombiner/job"
   "net/http"
-  "fmt"
   "encoding/json"
 )
 
@@ -12,14 +13,15 @@ type CombinerServer struct {}
 
 var InvalidParamsMessage = "{\"response\":\"invalid params\"}"
 
+// Turn the JSON body into a *Job struct, setting err if necessary.
 func decodeParams(r *http.Request) (j *job.Job, err error){
   j = &job.Job{}
-  decoder := json.NewDecoder(r.Body)
-  err = decoder.Decode(j)
-  fmt.Println(j)
+  err = json.NewDecoder(r.Body).Decode(j)
   return
 }
 
+// Handler to recieve a POSTed JSON body encoding a Job and if it validates,
+// send it along to be fulfilled.
 func (c CombinerServer) ProcessJob(w http.ResponseWriter, r *http.Request) {
   job, err := decodeParams(r)
   if err != nil || !job.IsValid() {
@@ -29,5 +31,6 @@ func (c CombinerServer) ProcessJob(w http.ResponseWriter, r *http.Request) {
   go combiner.Combine(job)
 }
 
+// No-op handler for responding to things like health checks.
 func (c CombinerServer) Ping(w http.ResponseWriter, r *http.Request) {}
 
