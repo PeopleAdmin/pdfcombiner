fe8095eef971324fb06567f4c895d6f73e85d524
Carl Brasic <carl.brasic@peopleadmin.com>
2013-08-07 20:14:12 -0500

    Documentation

--- cpdf/cpdf.go
+++ cpdf/cpdf.go
@@ -1,3 +1,4 @@
+// Package cpdf contains methods to manipulate pdf files using cpdf.
 package cpdf
 
 import(
@@ -5,7 +6,9 @@ import(
   "log"
 )
 
-func cpdfArgs(doclist []string) (args []string) {
+// Given a slice of documents, construct an args slice suitable for
+// passing to 'cpdf merge'.
+func cpdfMergeArgs(doclist []string) (args []string) {
   args = []string{"merge"}
   for _,doc := range doclist{
     args = append(args, doc)
@@ -14,12 +17,13 @@ func cpdfArgs(doclist []string) (args []string) {
   return
 }
 
+// Combine the files located at the specified paths into a single pdf.
 func Merge(doclist []string) {
   cpdf, err := exec.LookPath("cpdf")
   if err != nil { log.Fatal("no cpdf") }
   combine_cmd := exec.Command(cpdf)
   combine_cmd.Dir = "/tmp"
-  combine_cmd.Args = cpdfArgs(doclist)
+  combine_cmd.Args = cpdfMergeArgs(doclist)
   out, err := combine_cmd.Output()
   log.Println("stdout of command:",string(out))
 }
