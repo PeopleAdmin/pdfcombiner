12c7bc2bf0b179144a33a788f695fe79a7a9e479
Carl Brasic <carl.brasic@peopleadmin.com>
2013-08-10 13:46:46 -0500

    Fail fast if AWS credentials are not set.

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 // Accept requests to combine PDF documents located on Amazon S3 into
 // a single document, upload back to S3, and notify a callback URL.
+// TODO fail fast if S3 connection is not usable.
 package main
 
-
 import (
+	"launchpad.net/goamz/aws"
 	"net/http"
 	"pdfcombiner/server"
 )
+
+// Verify AWS credentials are set in the environment:
+// - AWS_ACCESS_KEY_ID
+// - AWS_SECRET_ACCESS_KEY
+func init() {
+	_, err := aws.EnvAuth()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	server := new(server.CombinerServer)
