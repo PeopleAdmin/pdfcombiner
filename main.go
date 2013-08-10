6cc0940c53567d1ed75f4fe27782df01fdad8bf6
Carl Brasic <carl.brasic@peopleadmin.com>
2013-08-10 12:28:20 -0500

    Leave out the GMAXPROCS for now
    The only cpu-bound components of this program are in exec() blocks,
    so the extra overhead is not worth it.

--- main.go
+++ main.go
@@ -2,17 +2,9 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"pdfcombiner/server"
-	"runtime"
 )
-
-func init() {
-	cpus := runtime.NumCPU()
-	fmt.Println("init with %s cpus", cpus)
-	runtime.GOMAXPROCS(cpus)
-}
 
 func main() {
 	server := new(server.CombinerServer)
