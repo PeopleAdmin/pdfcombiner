5211d9dcbf9276a351b4256ff606607e162f6233
Carl Brasic <carl.brasic@peopleadmin.com>
2013-08-11 17:11:11 -0500

    Add flag parsing for combining on command line

--- main.go
+++ main.go
@@ -7,25 +7,48 @@ package main
 import (
 	"flag"
 	"launchpad.net/goamz/aws"
+	"os"
 	"pdfcombiner/server"
 )
 
 var (
 	port       string
 	serverMode bool
+	bucket     string
+	employerId int
 )
 
 func init() {
 	flag.BoolVar(&serverMode, "server", false, "run in server mode")
-	flag.StringVar(&port, "port", "8080", "port to listen on")
+	flag.StringVar(&port, "port", "8080", "port to listen on for server mode")
+	flag.StringVar(&bucket, "bucket", "", "bucket name to use in standalone mode")
+	flag.IntVar(&employerId, "employer", 0, "id number of the employer to combine for in standalone mode")
+	flag.Parse()
+	flag.Usage = func() {
+		println("Usage: pdfcombiner [OPTS] [FILE...]")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 }
 
 func main() {
-	flag.Parse()
 	verifyAws()
-	if serverMode {
+	switch {
+	case serverMode:
 		server.ListenOn(port)
+	default:
+		combineSynchronously()
 	}
+}
+
+// Combine the requested files and return the result to standard out.
+func combineSynchronously() {
+	if flag.NArg() < 1 {
+		println("Cannot start in standalone mode with no files to combine.")
+		flag.Usage()
+	}
+	pdfFiles := flag.Args()
+	println(pdfFiles)
 }
 
 // Verify AWS credentials are set in the environment:
