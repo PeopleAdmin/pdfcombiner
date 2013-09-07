47d4e0a439267739b422d99c33270bd7b4439812
Carl Brasic <carl.brasic@peopleadmin.com>
2013-09-07 13:01:13 -0500

    doc

--- job/document.go
+++ job/document.go
@@ -4,14 +4,21 @@ import (
 	"github.com/PeopleAdmin/pdfcombiner/stat"
 )
 
-// A Document has a (file) name and a human readable title, possibly
-// used for watermarking prior to combination.
+// A Document is a reference to one part of a combined PDF.
+// it is is identified by its Key field, which is required.  It can also
+// have a Title, used for TOC bookmarks in the final combined document.
+// The actual PDF data comes from one of two places:
+//  - If the Data field is empty, the Key is treated as a S3 key that is
+//    fetched from the enclosing job's bucket.
+//  - If the Data field is nonempty, it must contain a zlib-compressed
+//    and Base64-encoded string containing the PDF.
 type Document struct {
 	Key   string `json:"key"`
 	Title string `json:"title"`
 	Data  string `json:"data,omitempty"`
 }
 
+// The Key is the only required field
 func (doc *Document) isValid() bool {
 	return doc.Key != ""
 }
