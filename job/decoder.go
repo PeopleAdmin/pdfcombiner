02e7c40cebf257a66d58e150c5c03e0627c8ec2f
Carl Brasic <carl.brasic@peopleadmin.com>
2013-09-07 13:45:14 -0500

    comments

--- job/decoder.go
+++ job/decoder.go
@@ -1,3 +1,7 @@
+// Document structs can either point to a url reference where the pdf is
+// stored, or contain a blob of the data itself.  The blob is compressed with
+// zlib and then wrapped in Base64.  These functions convert the encoded string
+// to a []byte containing the pdf.
 package job
 
 import (
@@ -8,8 +12,8 @@ import (
 	"strings"
 )
 
-// decodeEmbeddedData takes a base64-encoded string of a gzipped document
-// and returns the original source pdf as a byte slice.
+// decodeEmbeddedData takes a base64-encoded string of a gzipped document and
+// returns the original source pdf as a byte slice.
 func decodeEmbeddedData(encoded string) (decoded []byte, err error) {
 	pipeline, err := zlib.NewReader(decoder(encoded))
 	defer pipeline.Close()
